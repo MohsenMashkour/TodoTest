cababf4598814487e58d050e34acb02b247dbd2a
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2023-09-06 17:05:03 +0200

    changes

--- app/src/main/java/com/mkrdeveloper/todotest/MainActivity.kt
+++ app/src/main/java/com/mkrdeveloper/todotest/MainActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -14,11 +16,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissValue
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -27,7 +36,9 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -39,6 +50,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mkrdeveloper.todotest.ui.theme.TodoTestTheme
@@ -125,33 +137,72 @@ fun MyApp(modifier: Modifier = Modifier) {
             }
 
             LazyColumn(
-                Modifier.fillMaxSize()
+                state = rememberLazyListState(),
+                modifier = Modifier.fillMaxSize()
             ) {
 
                 items(todoList.size) { item ->
 
-                    Card(
-                        modifier.padding(horizontal = 20.dp, vertical = 6.dp)
-                    ) {
-                        Row(
-                            Modifier.fillMaxWidth(),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-
-                            var isChecked by remember {
-                                mutableStateOf(false)
+                    val state = rememberDismissState(
+                        confirmValueChange = {
+                            if (it == DismissValue.DismissedToStart) {
+                                todoList.removeAt(item)
                             }
-
-                            Checkbox(checked = isChecked, onCheckedChange = {
-                                isChecked = !isChecked
-                            })
-                            Spacer(modifier.width(16.dp))
-                            Text(text = todoList[item])
-
+                            true
                         }
-                    }
+                    )
+
+                    SwipeToDismiss(state = state, background = {
+                                             val color = when(state.dismissDirection){
+                                                 DismissDirection.StartToEnd-> Color.Green
+                                                 DismissDirection.EndToStart-> Color.Red
+                                                 null-> Color.Transparent
+                                             }
+                        Box(
+                            modifier
+                                .fillMaxSize()
+                                .background(color)
+                                .padding(horizontal = 20.dp, vertical = 6.dp)
+                        ){
+
+                            Icon(imageVector = Icons.Default.Delete, contentDescription ="delete", modifier.align(
+                                Alignment.CenterEnd
+                            ))
+                            Icon(imageVector = Icons.Default.Edit, contentDescription ="delete", modifier.align(
+                                Alignment.CenterStart
+                            ))
+                        }
+                    },
+                        dismissContent ={
+                            Card(
+                                modifier.padding(horizontal = 20.dp, vertical = 6.dp)
+                            ) {
+                                Row(
+                                    Modifier.fillMaxWidth(),
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+
+                                    var isChecked by remember {
+                                        mutableStateOf(false)
+                                    }
+
+                                    Checkbox(checked = isChecked, onCheckedChange = {
+                                        isChecked = !isChecked
+                                    })
+                                    Spacer(modifier.width(16.dp))
+                                    Text(text = todoList[item])
+
+                                }
+                            }
+                        },
+                        directions = setOf(
+                            DismissDirection.EndToStart,
+                            DismissDirection.StartToEnd
+                        )
+                    )
 
                 }
+
             }
         }
     }
